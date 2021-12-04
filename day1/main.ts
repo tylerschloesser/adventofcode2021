e0261c5aa073aa837e41c9ac2b31b579c8c4c802
Tyler Schloesser <tylerschloesser@gmail.com>
2021-12-04 07:59:16 -0800

    solve d1p1

--- day1/main.ts
+++ day1/main.ts
@@ -1,1 +1,20 @@
-console.log('hi')
+import fs from 'fs'
+
+const lines = fs
+  .readFileSync('input.txt', { encoding: 'utf8' })
+  .split('\n')
+  .slice(0, -1) // newline at end of file
+
+let data = lines.map((line) => parseInt(line))
+
+const { answer } = data.reduce<{ last: number; answer: number }>(
+  ({ last, answer }, v) => {
+    if (v > last) {
+      return { last: v, answer: answer + 1 }
+    }
+    return { last: v, answer }
+  },
+  { last: Number.MAX_SAFE_INTEGER, answer: 0 },
+)
+
+console.log(answer)
